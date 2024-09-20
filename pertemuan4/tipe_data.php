8a18ff17a2b24761e7132144889a0013d15f47db
hanoeyf <hanifahkurniasari0512@gmail.com>
2024-09-20 08:33:30 +0700

    praktikum 2

--- pertemuan4/tipe_data.php
+++ pertemuan4/tipe_data.php
@@ -12,4 +12,39 @@ echo "Variabel d: {$d} <br>";
 echo "Variabel e: {$e} <br>";
 
 var_dump($e);
+
+$nilaiMatematika = 5.1;
+$nilaiIPA = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
+
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
+
+$namaDepan = "Ibnu";
+$namaBelakang = "Jakaria";
+
+$namaLengkap = "{$namaDepan} {$namaBelakang} <br>";
+$namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
+
+echo "Nama Depan: {$namaDepan} <br>";
+echo "Nama Belakang: {$namaBelakang} <br>";
+
+echo $namaLengkap;
+
+$listMahasiswa = ["Wahid Abdullah", "Elmo Bachtiar", "Lendis Fabri"];
+echo $listMahasiswa[0];
+
 ?>
