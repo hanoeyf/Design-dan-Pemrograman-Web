a4909e477b85721baa6a2d2d530bd429e54ba5d4
hanoeyf <hanifahkurniasari0512@gmail.com>
2024-09-27 08:33:43 +0700

    percobaan 5 no.2

--- pertemuan4/array.php
+++ pertemuan4/array.php
@@ -7,4 +7,21 @@ $nilailulus = [];
     }
 }
 echo "Daftar nilai siswa yang lulus: ". implode(',', $nilailulus);
+
+$daftarkaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+    ];
+
+    $karyawanPengalamanLimaTahun = [];
+
+    foreach ($daftarkaryawan as $karyawan) {
+        if ($karyawan [1] > 5) {
+            $karyawanPengalamanLimaTahun[] = $karyawan[0];
+        }
+    }
+    echo "<br> Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: ". implode(',', $karyawanPengalamanLimaTahun);
 ?>
