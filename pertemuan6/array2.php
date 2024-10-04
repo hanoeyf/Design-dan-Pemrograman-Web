7073f1313e77a2ddf5fc99c4655be97b79e7fc75
hanoeyf <hanifahkurniasari0512@gmail.com>
2024-10-04 09:28:23 +0700

    indexed array dan Associative Array

---
+++ pertemuan6/array2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $dosen = [
+            'nama'=> 'Elok Nur Hamdana',
+            'domisili'=> 'Malang',
+            'jenis_Kelamin'=> 'perempuan'];
+
+        echo "Nama: {$dosen ['nama']} <br>";
+        echo "Domisili: {$dosen ['domisili']} <br>";
+        echo "Jenis Kelamin: {$dosen ['jenis_Kelamin']} <br>";
+
+        ?>
+
+    </body>
+</html>
