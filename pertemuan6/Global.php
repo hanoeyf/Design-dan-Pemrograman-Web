754b483d87b0f9ed7a146d0923d4d0962bab3601
NamaAnda <hanifahkurniasari0512@gmail.com>
2024-10-08 08:12:04 +0700

    tes

--- pertemuan6/Global.php
+++ pertemuan6/Global.php
@@ -9,4 +9,4 @@ function addition(){
 
 addition();
 echo $z;
-?>
+?> 
--- pertemuan6/Tugas/biodata.php
+++ pertemuan6/Tugas/biodata.php
@@ -10,20 +10,7 @@
                 $("#panel").slideToggle("slow");
             });
         });
-    </script>
-    <style> 
-        #panel, #flip {
-            padding: 5px;
-            text-align: center;
-            background-color: #e5eecc;
-            border: solid 1px #c3c3c3;
-        }
-
-        #panel {
-            padding: 20px;
-            display: none;
-        }
-    </style>
+     </script>
 </head>
 <body>
 <h2><strong>Data Siswa</strong></h2>
