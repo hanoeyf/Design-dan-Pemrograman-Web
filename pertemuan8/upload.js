e0254505eefdd398278c61920223d7b1616e8efc
NamaAnda <hanifahkurniasari0512@gmail.com>
2024-10-18 12:03:51 +0700

    Menghias Upload File

--- pertemuan8/upload.js
+++ pertemuan8/upload.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
+    $('#file').change(function(){
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
     $('#upload-form').submit(function(e){
         e.preventDefault();
 
         var formData = new FormData(this);
-            
+
         $.ajax({
             type: 'POST',
             url: 'upload_ajax.php',
@@ -11,11 +18,11 @@ $(document).ready(function(){
             cache: false,
             contentType: false,
             processData: false,
-            success: function (response) {
+            success: function(response) {
                 $('#status').html(response);
         },
         error: function(){
-                $('#status').html('Terjadi kesalahan saat mengunggah file.');
+            $('#status').html('Terjadi kesalahan saat mengunggah file.');
             }
         });
     });
