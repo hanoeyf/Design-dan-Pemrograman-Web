e03f39be3890f54794655225c01d1ef464b30178
NamaAnda <hanifahkurniasari0512@gmail.com>
2024-10-18 09:22:33 +0700

    upload jpg

--- pertemuan8/upload.php
+++ pertemuan8/upload.php
@@ -2,12 +2,23 @@
 if(isset($_POST["submit"])){ 
     $targetdir = "uploads.php"; //Direktori tujuan untuk menyimpan file
     $targetfile = $targetdir. basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){ 
-        echo "File berhasil diunggah.";
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5*1024*1024;
+
+    if(in_array($fileType, $allowedExtensions)&& $_FILES["myfile"]["size"]<=$maxsize)
+    {
+        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+            echo "File berhasil diunggah <br>";
+            echo "<img src='".$targetfile."' width='200' />";
+        }
+        else{ 
+            echo "Gagal mengunggah file.";
+        }
     }
-    else{ 
-        echo "Gagal mengunggah file.";
+    else{
+        echo "File tidak diizinkan atau ukuran file terlalu besar.";     
     }
 }
 ?>
